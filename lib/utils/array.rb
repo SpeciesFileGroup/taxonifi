004c443c2162d2a0aa21343317ba85e99e7d2da7
mjy <diapriid@gmail.com>
2013-01-17 12:04:49 -0600

    Abstract out general purpose array methods.

---
+++ lib/utils/array.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '../taxonifi'))
+
+# Generic Array methods
+module Taxonifi::Utils
+  module Array
+
+    # Return an Array of length size of black Arrays
+    def self.build_array_of_empty_arrays(size)
+      a = []
+      (0..(size-1)).each do |i|
+        a[i] = []
+      end
+      a
+    end
+
+  end # end Taxonifi::Utils::Array Module 
+end 
