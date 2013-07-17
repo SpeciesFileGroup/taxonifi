25ff2810a0a77f86c5068dc408cbce1aae799630
Matt Yoder <diapriid@gmail.com>
2013-07-17 12:30:03 -0500

    Clean up ref_collection authors.

--- lib/models/ref_collection.rb
+++ lib/models/ref_collection.rb
@@ -10,7 +10,7 @@ module Taxonifi
       # A Hash.  {:row_number => Ref
       attr_accessor :row_index
      
-      # Points a Ref#id to an array of Person#ids.  
+      # A Hash. Keys are  Ref#id, values are an Array of Person#ids.  
       # Built on request.
       attr_accessor :author_index 
 
@@ -38,7 +38,7 @@ module Taxonifi
       # This is only useful if you assume every author is unique.
       def enumerate_authors(initial_id = 0)
         i = initial_id 
-        collection.each do |r|
+        collection.each do ||r
           r.authors.each do |a|
             a.id = i
             i += 1
@@ -49,17 +49,86 @@ module Taxonifi
       # Finds unique authors, and combines them, then 
       # rebuilds author lists using references to the new unique set.
       def uniquify_authors(initial_id = 0)
+
+        matching_index = {
+          # ref_id => { 'ref_string_fingerprint' => [author_position in Ref.authors]} 
+        }
+
+        author_fingerprints = {}
+
+        # First pass, build matching array
+        collection.each do |r|
+          # Check for, and modify where necessary, Authors that are clearly not unique because
+          # they are replicated names in a author string, e.g. "Sweet and Sweet". 
+          matching_index[r.id] = {}
+          r.authors.each_with_index do |a,i|
+            id = a.compact_string
+            if matching_index[r.id][id]
+              matching_index[r.id][id].push(i)
+            else
+              matching_index[r.id][id] = [i]
+            end
+          end
+        end
+
+        # Next pass, modify names of necessarily unique authors so
+        # their fingerprint is unique.  Note we do not differentiate
+        # b/w sequential sets.
+        # E.g. if we have 5 names like so:
+        # Quate [1] and Quate [2]
+        # Quate [3], Smith [4] and Quate [5]
+        # Then [1,3], [2,5] become the same Person in this process.  We can not
+        # of course differentiate order, or if a 3rd "Quate" is present here given
+        # only this information.  Later on we might use Year of publication, or something
+        # similar to further "guess".
+        collection.each do |r|
+          matching_index[r.id].keys.each do |i|
+            if matching_index[r.id][i].size > 1
+              matching_index[r.id][i].each_with_index do |j,k|
+                # puts "uniquifying:" + "\_#{k}\_#{r.authors[j].last_name}"
+                r.authors[j].last_name = "\_#{k}\_#{r.authors[j].last_name}"
+              end
+            end
+          end
+        end
+
+        # Generate new authors based on identity 
+        authors = [] 
+        collection.each do |r|
+          r.authors.each do |a|
+            found = false
+            authors.each do |x|
+              if a.identical?(x)
+                found = true 
+                next           
+              end
+            end
+            if not found
+              authors.push a.clone
+            end
+          end
+        end
+
+        # Sequentially number the new authors, and index them.
         auth_index = {}
-        unique_authors.each_with_index do |a, i|
+        authors.each_with_index do |a, i|
           a.id = i + initial_id
           auth_index.merge!(a.compact_string => a)
         end
-        
+       
+        # Replace old authors with newly built/sequntially id'ed authors 
         collection.each do |r|
           new_authors = []
           r.authors.inject(new_authors){|ary, a| ary.push(auth_index[a.compact_string])}
           r.authors = new_authors
         end
+
+        # Remove the modifications that made authors unique 
+        # Crude to loop those unnecessary, but clean
+        authors.each do |a|
+          a.last_name.gsub!(/\_\d+\_/, '')
+        end
+
         true 
       end
 
@@ -71,7 +140,7 @@ module Taxonifi
         end
       end
 
-      # Return an array the unique author strings in this collection.
+      # Return an Array the unique author strings in this collection. 
       def unique_author_strings
         auths = {}
         collection.each do |r|
@@ -83,25 +152,18 @@ module Taxonifi
       end
 
       # Returns Array of Taxonifi::Model::Person
-      # Will need better indexing on big lists?
+      # !! Runs uniquify first. Careful, you might not want to do this
+      # !! unless you understand the consequences.
       def unique_authors
-        auths = []
-        collection.each do |r|
-          r.authors.each do |a|
-            found = false
-            auths.each do |x|
-              if a.identical?(x)
-                found = true 
-                next           
-              end
-            end
-            if not found
-              auths.push a.clone
-            end
-          end
-        end
-        auths
-      end
+        uniquify_authors
+        all_authors
+      end    
+
+
+      # Returns Array of Taxonifi::Model::Person
+      def all_authors
+        @collection.collect{|r| r.authors}.flatten.compact.uniq
+      end 
 
     end
   end
--- test/test_taxonifi_ref_collection.rb
+++ test/test_taxonifi_ref_collection.rb
@@ -1,7 +1,7 @@
 require 'helper'
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/models/ref_collection')) 
 
-class TestTaxonifiCollection < Test::Unit::TestCase
+class TestTaxonifiRefCollection < Test::Unit::TestCase
 
   def test_that_add_objects_adds_refs
     c = Taxonifi::Model::RefCollection.new
@@ -51,17 +51,54 @@ class TestTaxonifiCollection < Test::Unit::TestCase
   def test_uniquify_authors
     c = Taxonifi::Model::RefCollection.new
     ['Smith, A.R. 1920', 'Jones, A.R. and Smith, A.R. 1940', 'Jones, B. 1999', 'Jones, A. R. 1922', 'Jones, A. R., Smith, A.R. and Frank, A. 1943'].each do |a|
+      # smith ar
+      # jones ar
+      # jones b
+      # frank a
       n = Taxonifi::Model::Ref.new(:author_year => a)
       c.add_object(n)
     end
-    assert_equal 4, c.unique_authors.size
+   
+    assert_equal 8, c.all_authors.size
     assert_not_equal c.collection.first.authors.first, c.collection[1].authors.last
+    
     c.uniquify_authors(5)
+    assert_equal 4, c.all_authors.size
     assert_equal c.collection.first.authors.first, c.collection[1].authors.last
     assert_equal c.collection.first.authors.first, c.collection[4].authors[1]
     assert_equal c.collection[1].authors.first, c.collection[3].authors.first
     assert_equal 5, c.collection.first.authors.first.id 
   end
+
+  def test_uniquify_tricky_authors
+    c = Taxonifi::Model::RefCollection.new
+    ['Quate and Quate, 1920',].each do |a|
+      n = Taxonifi::Model::Ref.new(:author_year => a)
+      c.add_object(n)
+    end
+
+    assert_equal 2, c.unique_authors.size
+    c.uniquify_authors(0)
+    assert_equal 2, c.unique_authors.size
+    assert_not_equal c.collection.first.authors.first, c.collection.first.authors.last
+  end
+
+  def test_uniquify_trickier_authors
+    c = Taxonifi::Model::RefCollection.new
+    ['Quate and Quate, 1920', 'Quate, Smith and Quate, 1921', 'Smith, 1930', 'Quate, Quate, and Smith, 2000'].each do |a|
+      # Quate1
+      # Quate2
+      # Smith1
+      n = Taxonifi::Model::Ref.new(:author_year => a)
+      c.add_object(n)
+    end
+
+    assert_equal 9, c.all_authors.size
+    c.uniquify_authors(0)
+    assert_equal 3, c.unique_authors.size
+    # assert_not_equal c.collection.first.authors.first, c.collection.first.authors.last
+  end
+
 
 
 
